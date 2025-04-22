920cd118f8505aa2bfaa9626e435a0bb85e9aead
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-22 10:31:25 +0600

    search term more simplified

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,62 +2,46 @@ import { Prisma } from "@prisma/client";
 import prisma from "../../../shared/prisma";
 
 // search filter way : 1
-const getAdmins = async (searchTerm: any) => {
-  const filters: Prisma.AdminWhereInput[] = [];
-
-  if (searchTerm) {
-    filters.push({
-      OR: [
-        {
-          name: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    });
-  }
-  const where: Prisma.AdminWhereInput = { AND: filters };
-
-  const result = await prisma.admin.findMany({
-    where
-  });
-  return result;
-};
-
-// search filter way : 2
 // const getAdmins = async (searchTerm?: any) => {
-//   const where: Prisma.AdminWhereInput | undefined = searchTerm
-//     ? {
-//         OR: [
-//           {
-//             name: {
-//               contains: searchTerm,
-//               mode: "insensitive",
-//             },
-//           },
-//           {
-//             email: {
-//               contains: searchTerm,
-//               mode: "insensitive",
-//             },
-//           },
-//         ],
-//       }
-//     : undefined;
+//   const filters: Prisma.AdminWhereInput[] = [];
+
+//   if (searchTerm) {
+//     filters.push({
+//       OR: ["name", "email"].map((field) => ({
+//         [field]: {
+//           contains: searchTerm,
+//           mode: "insensitive",
+//         },
+//       })),
+//     });
+//   }
+//   const where: Prisma.AdminWhereInput = { AND: filters };
 
 //   const result = await prisma.admin.findMany({
 //     where,
 //   });
-
 //   return result;
 // };
+
+// search filter way : 2
+const getAdmins = async (searchTerm?: any) => {
+  const where: Prisma.AdminWhereInput | undefined = searchTerm
+    ? {
+        OR: ["name", "email"].map((field) => ({
+          [field]: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        })),
+      }
+    : undefined;
+
+  const result = await prisma.admin.findMany({
+    where,
+  });
+
+  return result;
+};
 
 export const adminService = {
   getAdmins,
