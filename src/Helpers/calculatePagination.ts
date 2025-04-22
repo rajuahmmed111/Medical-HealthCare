c29b9ff2572f5f603428268fd33b439786283a46
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-22 18:28:25 +0600

    pagination updaet

--- src/Helpers/calculatePagination.ts
+++ src/Helpers/calculatePagination.ts
@@ -1,9 +1,19 @@
-export const calculatedPagination = (options: {
+type IOptions = {
   page?: number;
   limit?: number;
   sortBy?: string;
   sortOrder?: string;
-}) => {
+};
+
+type IOptionsResult = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+};
+
+export const calculatedPagination = (options: IOptions): IOptionsResult => {
   const page = Number(options.page) || 1;
   const limit = Number(options.limit) || 10;
   const skip = (page - 1) * limit;
