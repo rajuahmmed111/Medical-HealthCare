969cad04c5ed80a72cae8f48c30e99848896398c
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-22 09:29:53 +0600

    search term updaate

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -4,10 +4,20 @@ import prisma from "../../../shared/prisma";
 const getAdmins = async (searchTerm: any) => {
   const result = await prisma.admin.findMany({
     where: {
-      name: {
-        contains: searchTerm || undefined,
-        mode: "insensitive",
-      },
+      OR: [
+        {
+          name: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
     },
   });
   return result;
