d679c161d1508425c8086e8426c72ffeb3df1712
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-22 10:14:08 +0600

    search term updaate

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,3 +1,4 @@
+import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { adminService } from "./admin.service";
@@ -5,16 +6,23 @@ import { Request, Response } from "express";
 
 // get admins
 const getAdmins = catchAsync(async (req: Request, res: Response) => {
+  try {
     const { searchTerm } = req.query;
-    // console.log(searchTerm);
-  const result = await adminService.getAdmins(searchTerm);
+    const result = await adminService.getAdmins(searchTerm);
 
-  sendResponse(res, {
-    statusCode: 200,
-    success: true,
-    message: "Admins retrieved successfully",
-    data: result,
-  });
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admins retrieved successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err?.message || "Failed to create admin",
+      error: err?.name,
+    });
+  }
 });
 
 export const adminController = {
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,25 +1,61 @@
+import { Prisma } from "@prisma/client";
 import prisma from "../../../shared/prisma";
 
-// get admins
-const getAdmins = async (searchTerm: any) => {
+// search filter way : 1
+// const getAdmins = async (searchTerm: any) => {
+//   const andCondition: Prisma.AdminWhereInput[] = [];
+
+//   if (searchTerm) {
+//     andCondition.push({
+//       OR: [
+//         {
+//           name: {
+//             contains: searchTerm,
+//             mode: "insensitive",
+//           },
+//         },
+//         {
+//           email: {
+//             contains: searchTerm,
+//             mode: "insensitive",
+//           },
+//         },
+//       ],
+//     });
+//   }
+//   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
+
+//   const result = await prisma.admin.findMany({
+//     where: whereCondition,
+//   });
+//   return result;
+// };
+
+// search filter way : 2
+const getAdmins = async (searchTerm?: any) => {
+  const where: Prisma.AdminWhereInput | undefined = searchTerm
+    ? {
+        OR: [
+          {
+            name: {
+              contains: searchTerm,
+              mode: "insensitive",
+            },
+          },
+          {
+            email: {
+              contains: searchTerm,
+              mode: "insensitive",
+            },
+          },
+        ],
+      }
+    : undefined;
+
   const result = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where,
   });
+
   return result;
 };
 
