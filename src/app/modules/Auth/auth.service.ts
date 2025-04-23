da50ac5e28fda0d11110cea4e8ff5724303c6f78
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-23 16:17:46 +0600

    generate accesstoken

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,6 +2,7 @@ import httpStatus from "http-status";
 import ApiError from "../../../Error/apiError";
 import prisma from "../../../shared/prisma";
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // login user
 const loginUser = async (email: string, password: string) => {
@@ -14,10 +15,31 @@ const loginUser = async (email: string, password: string) => {
     throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
 
-  const isPasswordMatch: boolean = await bcrypt.compare(password, userData.password);
+  const isPasswordMatch: boolean = await bcrypt.compare(
+    password,
+    userData.password
+  );
   if (!isPasswordMatch) {
     throw new ApiError(httpStatus.UNAUTHORIZED, "Password is incorrect");
   }
+
+  const accessToken = jwt.sign(
+    { id: userData.id, email: userData.email, role: userData.role },
+    "abcdefg",
+    { algorithm: "HS256", expiresIn: "1h" }
+  );
+  if (!accessToken) {
+    throw new ApiError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      "Token generation failed"
+    );
+  }
+
+  
+    return {
+        accessToken,
+        needPasswordChange: userData.needPasswordChange,
+    }
 };
 
 export const AuthService = {
