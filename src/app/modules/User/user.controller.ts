51d2d4c179ebd5a6a427ea21df0dcca6fbfdcc28
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-23 11:16:10 +0600

    redanduncy code remove from user controler

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -6,22 +6,14 @@ import { Request, Response } from "express";
 
 // create admin
 const createAdmin = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const result = await userService.createAdmin(req.body);
+  const result = await userService.createAdmin(req.body);
 
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: "Admin created successfully",
-      data: result,
-    });
-  } catch (err) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err?.message || "Failed to create admin",
-      error: err?.name,
-    });
-  }
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Admin created successfully",
+    data: result,
+  });
 });
 
 export const userController = {
