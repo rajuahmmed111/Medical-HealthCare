a480d49d4c65ebcc157ad64615d2083f73f7c2a4
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-23 11:03:31 +0600

    add not found api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import httpStatus from "http-status";
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import router from "./app/Routes";
-import GlobalErrorHandler from "./app/modules/globalErrorHandler";
+import ApiNotFound from "./app/Middleware/apiNotFound";
+import GlobalErrorHandler from "./app/Middleware/globalErrorHandler";
 
 app.use(cors());
 
@@ -10,14 +12,27 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Developer!");
+});
+
 // Setup API routes
 app.use("/api/v1", router);
 
 // Global error handler
 app.use(GlobalErrorHandler);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Developer!");
+// Handle 404 errors
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      method: req.method,
+      message: "Your requested path is not found!",
+    },
+  });
 });
 
 export default app;
