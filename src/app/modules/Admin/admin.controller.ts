53eecc4e6f5f116c4b9f55779365f5640d57d705
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-23 11:15:08 +0600

    redanduncy code remove from admin controler

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -8,26 +8,18 @@ import { filterField, paginationField } from "./admin.constant";
 
 // get admins
 const getAdmins = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const filter = pick(req.query, filterField);
-    const options = pick(req.query, paginationField);
+  const filter = pick(req.query, filterField);
+  const options = pick(req.query, paginationField);
 
-    const result = await adminService.getAdmins(filter, options);
+  const result = await adminService.getAdmins(filter, options);
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admins retrieved successfully",
-      data: result.data,
-      meta: result.meta,
-    });
-  } catch (err) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err?.message || "Failed to create admin",
-      error: err?.name,
-    });
-  }
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admins retrieved successfully",
+    data: result.data,
+    meta: result.meta,
+  });
 });
 
 // gets admin by id
@@ -45,69 +37,44 @@ const getAdminById = catchAsync(async (req: Request, res: Response) => {
 
 // update  admin by id
 const updateAdminById = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const data = req.body;
+  const { id } = req.params;
+  const data = req.body;
 
-    const result = await adminService.updateAdminByIdIntoDB(id, data);
+  const result = await adminService.updateAdminByIdIntoDB(id, data);
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin updated successfully",
-      data: result,
-    });
-  } catch (err) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err?.message || "Failed to create admin",
-      error: err?.name,
-    });
-  }
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin updated successfully",
+    data: result,
+  });
 });
 
 // delete admin by id
 const deleteAdminById = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const result = await adminService.deleteAdminByIdFromDB(id);
+  const { id } = req.params;
+  const result = await adminService.deleteAdminByIdFromDB(id);
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin deleted successfully",
-      data: result,
-    });
-  } catch (err) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err?.message || "Failed to create admin",
-      error: err?.name,
-    });
-  }
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin deleted successfully",
+    data: result,
+  });
 });
 
 // soft delete admin by id
-const softDeleteAdminById = catchAsync(
-  async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params;
-      const result = await adminService.softDeleteAdminByIdFromDB(id);
+const softDeleteAdminById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await adminService.softDeleteAdminByIdFromDB(id);
 
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Admin soft deleted successfully",
-        data: result,
-      });
-    } catch (err) {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        message: err?.message || "Failed to create admin",
-        error: err?.name,
-      });
-    }
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin soft deleted successfully",
+    data: result,
   });
+});
 
 export const adminController = {
   getAdmins,
