2c888d88137c8ced39565a9520f2f931a4439f21
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-14 23:39:29 +0600

    add comment in patient route

--- src/app/modules/Patient/patient.route.ts
+++ src/app/modules/Patient/patient.route.ts
@@ -3,25 +3,32 @@ import { PatientController } from './patient.controller';
 
 const router = express.Router();
 
+// get all
 router.get(
     '/',
     PatientController.getAllFromDB
 );
 
+
+// get single
 router.get(
     '/:id',
     PatientController.getByIdFromDB
 );
 
+// update patient
 router.patch(
     '/:id',
     PatientController.updateIntoDB
 );
 
+// delete patient
 router.delete(
     '/:id',
     PatientController.deleteFromDB
 );
+
+// soft delete
 router.delete(
     '/soft/:id',
     PatientController.softDelete
