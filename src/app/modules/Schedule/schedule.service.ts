750f6eab13e9a20a5d4ef3050f1170269519dac5
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-19 02:53:07 +0600

    create schedule

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,12 +1,15 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 
 // create schedule
 const createSchedule = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
 
-  const currentDate = new Date(startDate);
-  const lastDate = new Date(endDate);
-  // console.log(lastDate);
+  const schedules = [];
+  const intervalTime = 30;
+
+  const currentDate = new Date(startDate); // start date
+  const lastDate = new Date(endDate); // end date
 
   while (currentDate <= lastDate) {
     const startDateTime = new Date(
@@ -18,13 +21,32 @@ const createSchedule = async (payload: any) => {
 
     const endDataTime = new Date(
       addHours(
-        `${format(lastDate, "yyyy-MM-dd")}`,
+        `${format(currentDate, "yyyy-MM-dd")}`,
         Number(endTime.split(":")[0])
       )
     );
-    console.log(endDataTime);
 
+
+    // separate schedule slot
+    while (startDateTime < endDataTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, intervalTime),
+      };
+
+      // create schedule
+      const result = await prisma.schedule.create({
+        data: scheduleData,
+      });
+      schedules.push(result);
+
+      startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
   }
+
+  return schedules;
 };
 
 export const ScheduleService = {
