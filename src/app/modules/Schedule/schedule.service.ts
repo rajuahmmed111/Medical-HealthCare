47cdeff3978b146e20ce176bf7ede1f03ceb5b23
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-19 02:04:10 +0600

    in schedule convert time

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,12 +1,30 @@
-import prisma from "../../../shared/prisma";
+import { addHours, format } from "date-fns";
 
 // create schedule
 const createSchedule = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
-  console.log(payload);
 
-  // 
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+  // console.log(lastDate);
 
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
+
+    const endDataTime = new Date(
+      addHours(
+        `${format(lastDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
+    console.log(endDataTime);
+
+  }
 };
 
 export const ScheduleService = {
